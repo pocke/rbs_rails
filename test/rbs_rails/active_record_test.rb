76fdeb24a280c9339f0a79cc606c2e4726ece946
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-07-23 19:25:19 +0900

    minimum test

---
+++ test/rbs_rails/active_record_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class ActiveRecordTest < Minitest::Test
+  def test_type_check
+    clean_test_signatures
+
+    signature_path = Pathname(File.expand_path('../sig', __dir__))
+    RbsRails.copy_signatures(to: signature_path)
+
+    sh!('steep', 'check', chdir: File.expand_path('../app', __dir__))
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,16 @@
 require 'minitest'
 require 'minitest/autorun'
 
+require 'pathname'
 require 'rbs_rails'
+
+TEST_SIG_DIR = Pathname(File.expand_path('./sig', __dir__))
+
+def clean_test_signatures
+  File.delete(*TEST_SIG_DIR.glob('**/*.rbs'))
+end
+
+def sh!(*commands, **kw)
+  puts '$ ' + commands.join(' ')
+  system(*commands, exception: true, **kw)
+end
