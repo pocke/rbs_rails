e3efb51aa60281279df01622de64de0ca519c129
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-07-28 00:34:11 +0900

    WIP

--- test/rbs_rails/active_record_test.rb
+++ test/rbs_rails/active_record_test.rb
@@ -7,6 +7,9 @@ class ActiveRecordTest < Minitest::Test
     signature_path = Pathname(File.expand_path('../sig', __dir__))
     RbsRails.copy_signatures(to: signature_path)
 
+    Bundler.with_unbundled_env do
+      sh!('bin/rake', 'generate_rbs_for_model', chdir: File.expand_path('../app', __dir__))
+    end
     sh!('steep', 'check', chdir: File.expand_path('../app', __dir__))
   end
 end
