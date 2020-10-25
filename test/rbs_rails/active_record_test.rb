2d50d0dae30c138e9146398537877f7cc01a1452
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-10-25 17:36:13 +0900

    Prepare test environment

--- test/rbs_rails/active_record_test.rb
+++ test/rbs_rails/active_record_test.rb
@@ -7,9 +7,13 @@ class ActiveRecordTest < Minitest::Test
     signature_path = Pathname(File.expand_path('../sig', __dir__))
     RbsRails.copy_signatures(to: signature_path)
 
+    dir = File.expand_path('../app', __dir__)
+
     Bundler.with_unbundled_env do
-      sh!('bin/rake', 'generate_rbs_for_model', chdir: File.expand_path('../app', __dir__))
+      sh!('bundle', 'install', chdir: dir)
+      sh!('bin/rake', 'db:create', 'db:schema:load', chdir: dir)
+      sh!('bin/rake', 'generate_rbs_for_model', chdir: dir)
     end
-    sh!('steep', 'check', chdir: File.expand_path('../app', __dir__))
+    sh!('steep', 'check', chdir: dir)
   end
 end
