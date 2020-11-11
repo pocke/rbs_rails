564c1834ca8f82cebd29adf7311cebfe0066e0f9
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-11-12 00:39:27 +0900

    RBS v16 for actionpack

--- bin/generate_rbs_from_rails_source_code.rb
+++ bin/generate_rbs_from_rails_source_code.rb
@@ -22,10 +22,26 @@ def sh!(*cmd, **kwargs)
   end
 end
 
+def patch!(name, rbs)
+  case name
+  when 'actionpack'
+    rbs.gsub!(/(      def _reduce_\d+: \(untyped val, untyped _values\) -> )([A-Z]\w*)/) do
+      "#{$~[1]}Nodes::#{$~[2]}"
+    end || raise('it looks unnecessary.')
+
+    # XXX: I guess add-type-params.rb resolves this
+    rbs.gsub!('-> Parameter', '-> Parameter[untyped]') || raise('it looks unnecessary')
+  end
+end
+
 def main(rails_code_dir, name)
   files = Pathname(rails_code_dir).join(name, 'lib').glob('**/*.rb').map(&:to_s)
   generated_rbs_path = SIG_DIR.join("#{name}.rbs")
+
   rbs = sh! 'ruby', bin("rbs-prototype-rb.rb"), 'prototype', 'rb', *files
+  rbs.gsub!(/^(?<indent>\s*)class (?<name>\S+) < $/) { "#{$~[:indent]}class #{$~[:name]} # Note: It inherits unnamed class, but omitted" }
+  rbs.gsub!(/[^[:ascii:]]+/, '(trim non-ascii characters)')
+  patch! name, rbs
   generated_rbs_path.write(rbs)
 
   rbs = sh! 'ruby', bin('add-type-params.rb'), generated_rbs_path.to_s
