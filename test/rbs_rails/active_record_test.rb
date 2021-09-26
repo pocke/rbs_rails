479d3fe972572109175bb0f1c4dc2ecced6e68d0
ksss <co000ri@gmail.com>
2021-09-26 16:28:36 +0900

    Support `has_secure_password`

--- test/rbs_rails/active_record_test.rb
+++ test/rbs_rails/active_record_test.rb
@@ -106,6 +106,17 @@ class ActiveRecordTest < Minitest::Test
         def restore_updated_at!: () -> void
         def clear_updated_at_change: () -> void
 
+        module ::ActiveModel::SecurePassword::InstanceMethodsOnActivation_password
+          def password=: (String) -> String
+
+          def password_confirmation=: (String) -> String
+
+          def authenticate_password: (String) -> (User | false)
+
+          alias authenticate authenticate_password
+        end
+        include ::ActiveModel::SecurePassword::InstanceMethodsOnActivation_password
+
         def temporary!: () -> bool
         def temporary?: () -> bool
         def accepted!: () -> bool
