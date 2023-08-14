581ce45d1e9fb1e9824aedbef1011997e637510a
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-08-14 14:19:33 +0900

    test: Rename test/app to test/rails6
    As a preparation of adding testcases for Rails7, the `test/app`
    directory is now renamed to `test/rails6`.  It's still needed to support
    Rails5 styled enum feature.

--- test/rbs_rails/active_record_test.rb
+++ test/rbs_rails/active_record_test.rb
@@ -4,18 +4,18 @@ class ActiveRecordTest < Minitest::Test
   def test_type_check
     clean_test_signatures
 
-    setup!
+    setup!(rails6_app_dir)
 
-    dir = app_dir
+    dir = rails6_app_dir
     sh!('steep', 'check', chdir: dir)
   end
 
   def test_user_model_rbs_snapshot
     clean_test_signatures
 
-    setup!
+    setup!(rails6_app_dir)
 
-    rbs_path = app_dir.join('sig/rbs_rails/app/models/user.rbs')
+    rbs_path = rails6_app_dir.join('sig/rbs_rails/app/models/user.rbs')
     expect_path = expectations_dir / 'user.rbs'
     # Code to re-generate the expectation files
     # expect_path.write rbs_path.read
@@ -23,17 +23,15 @@ class ActiveRecordTest < Minitest::Test
     assert_equal expect_path.read, rbs_path.read
   end
 
-  def app_dir
-    Pathname(__dir__).join('../app')
+  def rails6_app_dir
+    Pathname(__dir__).join('../rails6')
   end
 
   def expectations_dir
     Pathname(__dir__).join('../expectations')
   end
 
-  def setup!
-    dir = app_dir
-
+  def setup!(dir)
     Bundler.with_unbundled_env do
       sh!('bundle', 'install', chdir: dir)
       sh!('bin/rake', 'db:create', 'db:schema:load', chdir: dir)
