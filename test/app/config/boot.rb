b885b05350e28578bee1a577a596d79a05148eb2
Takeshi KOMIYA <i.tkomiya@gmail.com>
2025-05-02 20:38:06 +0900

    test: Upgrade testing Rails app to 7.0
    We determined to upgrade the testing Rails app to the newer version of
    Ruby on Rails.  We still keep supporting the older version of Rails.
    But we use the newer version of Rails for the testing.
    
    As a first step, this upgrades the Rails app for testing to v7.0.

--- test/app/config/boot.rb
+++ test/app/config/boot.rb
@@ -1,5 +1,3 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-# Disable Bootsnap due to https://github.com/pocke/rbs_rails/pull/198#issuecomment-931215730
-# require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+require "bundler/setup" # Set up gems listed in the Gemfile.
