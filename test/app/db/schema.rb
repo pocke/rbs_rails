b9691d38881a07123e601547f517fc938a6a2270
Takeshi KOMIYA <i.tkomiya@gmail.com>
2025-05-03 01:24:50 +0900

    test: Upgrade testing Rails app to 7.2
    We determined to upgrade the testing Rails app to the newer version of
    Ruby on Rails.  We still keep supporting the older version of Rails.
    But we use the newer version of Rails for the testing.

--- test/app/db/schema.rb
+++ test/app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_02_113353) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_02_113353) do
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
     t.string "description", null: false
@@ -35,5 +35,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_02_113353) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
