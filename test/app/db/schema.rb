c1356ac9d0f7e34282541a146e53d12826f225a6
Takeshi KOMIYA <i.tkomiya@gmail.com>
2025-05-02 23:25:43 +0900

    test: Upgrading testing Rails ap to 7.1
    We determined to upgrade the testing Rails app to the newer version of
    Ruby on Rails.  We still supporting the older version of Rails.
    But we use the newer version of Rails for the testing.

--- test/app/db/schema.rb
+++ test/app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_06_24_104302) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_24_104302) do
   create_table "audits", force: :cascade do |t|
     t.integer "auditable_id"
     t.string "auditable_type"
