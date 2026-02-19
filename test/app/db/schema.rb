5f51876c01dc0ddb956499bd33e88556a22e3ed2
rhiroe <ride.poke@gmail.com>
2026-02-19 12:44:50 +0000

    Add support for composed_of aggregation RBS generation
        Generate reader/writer method signatures for ActiveRecord composed_of
        aggregations, with optional type support when allow_nil is specified.

--- test/app/db/schema.rb
+++ test/app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_05_084032) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_19_122811) do
   create_table "audits", force: :cascade do |t|
     t.integer "auditable_id"
     t.string "auditable_type"
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_05_084032) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "currency_code", null: false
   end
 
   create_table "groups", force: :cascade do |t|
